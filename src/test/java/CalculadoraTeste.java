82fe7f18d0feaabae4db6aacb3513b01736a3604
fesilva2109 <felipesilvaa.2109@gmail.com>
2025-03-13 22:20:31 -0300

    Iniciando com o BeforeEach

--- src/test/java/CalculadoraTeste.java
+++ src/test/java/CalculadoraTeste.java
@@ -1,5 +1,11 @@
+import Calculadora.Calculadora;
+import org.junit.jupiter.api.BeforeEach;
+
 public class CalculadoraTeste {
     private Calculadora calc;
-    
+    @BeforeEach
+    public void inicializar(){
+        this.calc = new Calculadora();
+    }
 
 }
