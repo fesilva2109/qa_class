48925f553b16953d3caa2cda59679c96331a1757
fesilva2109 <felipesilvaa.2109@gmail.com>
2025-03-13 22:00:43 -0300

    Criando método testAssertArrayEquals

--- src/test/java/TestQa.java
+++ src/test/java/TestQa.java
@@ -1,7 +1,13 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 public class TestQa {
     @Test
     public void testAssertArrayEquals(){
+        byte[] esperado = "teste".getBytes();
+        byte[] atual = "teste".getBytes();
+        assertArrayEquals(esperado,atual);
+
     }
 }
