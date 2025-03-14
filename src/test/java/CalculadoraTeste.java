77077c0ee15fb628a4668e87463e1afecfa562ec
fesilva2109 <felipesilvaa.2109@gmail.com>
2025-03-13 22:22:12 -0300

    Utiliando testeSomar

--- src/test/java/CalculadoraTeste.java
+++ src/test/java/CalculadoraTeste.java
@@ -1,11 +1,21 @@
 import Calculadora.Calculadora;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculadoraTeste {
     private Calculadora calc;
     @BeforeEach
     public void inicializar(){
         this.calc = new Calculadora();
+    }
+    @Test
+    void testSomar(){
+        int a =3;
+        int b= 2;
+        int resultado = calc.somar(a,b);
+        assertEquals(a+b,resultado);
     }
 
 }
