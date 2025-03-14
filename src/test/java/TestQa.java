5f4eb7ea11618e9ac6817d265e05daccc7b6a2c5
fesilva2109 <felipesilvaa.2109@gmail.com>
2025-03-13 22:25:34 -0300

    Criando testAssertNull

--- src/test/java/TestQa.java
+++ src/test/java/TestQa.java
@@ -1,7 +1,6 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestQa {
     @Test
@@ -14,6 +13,10 @@ public class TestQa {
     @Test
     public void testAssertEquals(){
         assertEquals('text','text');
+    }
+    @Test
+    public void testAssertNull(){
+        assertNull(null);
     }
 
 
